4ceddf398302134965e695ae1dcf94d1274645a5
shrgo <shuangren.gou@gmail.com>
2022-06-26 22:54:31 +0800

    Add TimerQueue test code.

---
+++ example/simple/TimerQueueExample.cc
@@ -0,0 +1,49 @@
+// Copyright (c) 2022 shr-go. All rights reserved.
+// Author: shr-go
+
+#include "gg_lib/net/EventLoop.h"
+#include "gg_lib/net/TimerQueue.h"
+#include "gg_lib/Logging.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <random>
+
+using namespace gg_lib;
+using namespace gg_lib::net;
+
+int cnt = 0;
+EventLoop *g_loop;
+
+void addCnt(int n) {
+    cnt += n;
+    g_loop->runAfter(1, std::bind(&addCnt, 1));
+}
+
+void print() {
+    LOG_INFO << Fmt("Cnt = {}", cnt);
+}
+
+void stop(TimerId timer) {
+    LOG_INFO << Fmt("Stop Timer = {}", timer);
+    g_loop->cancel(timer);
+}
+
+void cancel() {
+    LOG_INFO << "Cancel loop";
+    g_loop->quit();
+}
+
+int main() {
+    EventLoop loop;
+    g_loop = &loop;
+    for (int i = 0; i < 100000; ++i) {
+        loop.runAfter((1.0 / (i + 1)), std::bind(&addCnt, 1));
+    }
+    TimerId timer = loop.runEvery(1, &print);
+    LOG_INFO << Fmt("Begin Timer = {}", timer);
+    loop.runAfter(14, std::bind(&stop, timer));
+    loop.runAfter(16, &cancel);
+    loop.loop();
+    LOG_INFO << "main loop exits";
+}
---
+++ test/net/TimerQueueTest.cc
@@ -0,0 +1,47 @@
+// Copyright (c) 2022 shr-go. All rights reserved.
+// Author: shr-go
+
+#include "gg_lib/net/EventLoop.h"
+#include "gg_lib/net/TimerQueue.h"
+#include "gg_lib/Logging.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace gg_lib;
+using namespace gg_lib::net;
+
+int cnt = 0;
+EventLoop *g_loop;
+
+void print(const char *msg) {
+    printf("msg %s %s\n", Timestamp::now().toString().c_str(), msg);
+    if (++cnt == 20) {
+        g_loop->quit();
+    }
+}
+
+void cancel(TimerId timer) {
+    g_loop->cancel(timer);
+    printf("cancelled at %s\n", Timestamp::now().toString().c_str());
+}
+
+int main() {
+    EventLoop loop;
+    g_loop = &loop;
+    Logger::setLogLevel(Logger::TRACE);
+    print("main");
+    loop.runAfter(1, std::bind(print, "once1"));
+    loop.runAfter(1.5, std::bind(print, "once1.5"));
+    loop.runAfter(2.5, std::bind(print, "once2.5"));
+    loop.runAfter(3.5, std::bind(print, "once3.5"));
+    TimerId t45 = loop.runAfter(4.5, std::bind(print, "once4.5"));
+    loop.runAfter(4.2, std::bind(cancel, t45));
+    loop.runAfter(4.8, std::bind(cancel, t45));
+    loop.runEvery(2, std::bind(print, "every2"));
+    TimerId t3 = loop.runEvery(3, std::bind(print, "every3"));
+    loop.runAfter(9.001, std::bind(cancel, t3));
+
+    loop.loop();
+    print("main loop exits");
+}
